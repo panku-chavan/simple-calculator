a0dc892962c80d5cf85842ef1e428c1b0b4bd109
panku-chavan <pankajchavan963@gmail.com>
2023-02-12 13:47:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { Calclulator } from './Components/Calculator';
+
+
+function App() {
+  return (
+    <div>
+        <h1  className="App">Simple Calclulator</h1>
+        
+        <Calclulator/>
+    </div>
+  );
+}
+
+export default App;
